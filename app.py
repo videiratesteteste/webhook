8af7f95bab095a14f111453fe16312556c9812d0
videiratesteteste <leoytmusic96@gmail.com>
2024-09-30 11:06:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 def pagina_dashboard():
     return '<h1>Webhook Z-API</h1>'
 
-@app.route('/rec_msg', methods=['POST'])
+@app.route('/rec_msg', methods=['PUT'])
 def webhook():
     data = request.get_json()
     print("Dados recebidos:", data)
     return jsonify({'status': 'success'}), 200
 
+@app.route('/msg_env', methods=['PUT'])
+def webhook():
+    data = request.get_json()
+    print("Dados enviados:", data)
+    return jsonify({'status': 'success'}), 200
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=True)
